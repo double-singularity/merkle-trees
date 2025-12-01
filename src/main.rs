3b6be0213bc9b07852d29de4f162efcee1ebd673
panzer <rstu1081@gmail.com>
2025-12-01 20:49:41 +0700

    Add hash function using sha256

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use sha2::{Sha256, Digest};
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hash("hello world"));
+}
+
+fn hash(data: &str) -> String {
+    let mut hasher = Sha256::new();
+
+    hasher.update(data);
+
+    let result = hasher.finalize();
+
+    format!("{:x}", result)
 }
